e586a1619efe0b3bb253e9d36108cf3ae594820b
ElizaSitmukhanova <eliza.sit@mail.ru>
2023-04-05 16:53:08 +0300

    cycles

---
+++ practice.js
@@ -0,0 +1,64 @@
+"use strict";
+
+/* let incr = 10,
+     decr = 10; */
+//incr++;
+//decr--;
+/* console.log(incr++);
+console.log(--decr);
+
+console.log(2+2*2!=8);
+
+
+const isChecked = false,
+     isClose = false;
+console.log(isChecked || !isClose);
+
+ */
+
+let num = 50;
+/* while (num<55) {
+     console.log(num);
+     num++;
+}; */
+
+/* do {
+     console.log(num);
+     num++;
+}
+while (num<55); */
+
+/* for (let i=1; i<10; i++) {
+     if (i===3) {
+          break;
+          continue;
+     }
+     console.log(i);
+};
+
+function secondTask() {
+     // Пишем решение вот тут
+     for(let t=20; t>10; t--) {
+         if (t===13) {
+         break
+     } 
+     console.log(t)
+}
+ } */
+
+ /* function fourthTask() {
+     let i = 2;
+ 
+     while (i <= 16) {
+         if (i % 2 === 0) {
+             i++;
+             continue;
+         } else {
+             console.log(i);
+         }
+         i++;
+     }
+ }
+ 
+ fourthTask() */
+ 
