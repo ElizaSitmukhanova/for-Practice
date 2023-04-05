68be60d2b7d533f55ad840ceb41f0bce691ca01c
ElizaSitmukhanova <eliza.sit@mail.ru>
2023-04-05 17:23:18 +0300

    cycleInCycles

--- practice.js
+++ practice.js
@@ -1,64 +1,34 @@
 "use strict";
 
-/* let incr = 10,
-     decr = 10; */
-//incr++;
-//decr--;
-/* console.log(incr++);
-console.log(--decr);
 
-console.log(2+2*2!=8);
-
-
-const isChecked = false,
-     isClose = false;
-console.log(isChecked || !isClose);
-
- */
-
-let num = 50;
-/* while (num<55) {
-     console.log(num);
-     num++;
-}; */
-
-/* do {
-     console.log(num);
-     num++;
-}
-while (num<55); */
-
-/* for (let i=1; i<10; i++) {
-     if (i===3) {
-          break;
-          continue;
-     }
+for(let i = 0; i<3; i++) {
      console.log(i);
+     for(let j = 0; j<3; j++) {
+          console.log(j);
+     }
 };
 
-function secondTask() {
-     // Пишем решение вот тут
-     for(let t=20; t>10; t--) {
-         if (t===13) {
-         break
-     } 
-     console.log(t)
-}
- } */
+let result ='';
+const length = 7;
 
- /* function fourthTask() {
-     let i = 2;
- 
-     while (i <= 16) {
-         if (i % 2 === 0) {
-             i++;
-             continue;
-         } else {
-             console.log(i);
-         }
-         i++;
+for( let i =1; i<length; i++){
+     for( let j=0; j<i; j++){
+          result +='*';
      }
- }
- 
- fourthTask() */
- 
+     result += '\n'
+}
+
+console.log(result)
+
+first: for( let i =1; i<3; i++){
+    console.log(`first level ${i}`); 
+     for( let j=0; j<3; j++){
+          console.log(`second level ${j}`); 
+          for( let k=0; k<3; k++){
+               if(k===2) break first;
+               console.log(`third level ${k}`); 
+          }
+     }
+}
+
+console.log(result)
