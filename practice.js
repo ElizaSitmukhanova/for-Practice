be895f5615878730f7618f37da0eee3fff9657f6
ElizaSitmukhanova <eliza.sit@mail.ru>
2023-04-08 19:40:24 +0300

    figure

--- practice.js
+++ practice.js
@@ -1,42 +1,28 @@
-"use strict";
-
-
-function firstTask() {
-     const arr = [3, 5, 8, 16, 20, 23, 50];
-     let result = [];
-  for (let i=0; i<arr.length; i++) { 
-     result += arr[i] +','
-  }
-    
-     return console.log(result);
- }
-
-firstTask();
-
-/* function secondTask() {
-     // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-     const data = [5, 10, 'Shopping', 20, 'Homework'];
-     for(let i=0; i<data.length; i++) {
-          if (typeof(data[i])==='number') {
-               data[i]= data[i]*2
-          } else if (typeof(data[i]) ==='string') {
-               data[i]=`${data[i]} - done`;
-          }
-     }
-  console.log(data);
-  return data
- }
- secondTask()
- */
-
+/* 
  function thirdTask() {
      // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
      const data = [5, 10, 'Shopping', 20, 'Homework'];
      let result = [];
 
-     for(let i=0; i<data.length; i++) {
-          result += data[i];
+     for(let i=1; i<data.length; i++) {
+          result[i - 1] = data[data.length - i]
      }
   return console.log(result)
  }
- thirdTask()
+ thirdTask() */
+
+
+ const lines = 5;
+ let result = '';
+ 
+ for (let i = 0; i <= lines; i++) {
+     for (let j = 0; j < lines-i; j++) {
+         result += " ";
+     }
+     for (let j = 0; j < 2*i; j++) {
+         result += "*";
+     }
+     result += "\n";
+ }
+ 
+ console.log(result)
