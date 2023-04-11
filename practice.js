1510d2bc0c91fd6e83f191fb2ace2307fd7f189f
ElizaSitmukhanova <eliza.sit@mail.ru>
2023-04-11 14:35:00 +0300

    task8

--- practice.js
+++ practice.js
@@ -79,4 +79,49 @@ function getCoupeNumber(place) {
  
 }
 
-getCoupeNumber(33)
+getCoupeNumber(33);
+
+function getTimeFromMinutes(min) {
+     if (min < 0 || !Number.isInteger(min) || typeof (min) !== 'number') {
+          return console.log('Ошибка, проверьте данные')
+     }
+     let hour = Math.floor(min / 60);
+     let min2 = min - (hour * 60); // или использовать % остаток от деления
+     let hoursStr = '';
+
+     switch (hour) {
+          case 0: 
+              hoursStr = 'часов';
+              break;
+          case 1:
+              hoursStr = 'час';
+              break;
+          case 2:
+          case 3:
+          case 4:
+              hoursStr = 'часа';
+              break;
+          default:
+              hoursStr = 'часов';
+      }
+  
+      return console.log(`Это ${hour} ${hoursStr} и ${min2} минут`);
+  }
+
+getTimeFromMinutes(60);
+
+function findMaxNumber(a, b ,c, d) {
+     // Самое простое - это использовать Math.max :)
+     // А оптимизировать такую проверку мы научимся совсем скоро
+     if (typeof(a) !== 'number' ||
+         typeof(b) !== 'number' ||
+         typeof(c) !== 'number' ||
+         typeof(d) !== 'number') {
+         return console.log(0);
+     } else {
+         return console.log(Math.max(a, b ,c, d));
+     }
+ }
+ 
+ findMaxNumber(1, 5, 6.6, 10.5);
+ findMaxNumber(1, 5, '6', '10');
