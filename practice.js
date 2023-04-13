d3fa568d9d03942e55432c75e0221f5a450c53e0
ElizaSitmukhanova <eliza.sit@mail.ru>
2023-04-13 21:49:48 +0300

    spread op

--- practice.js
+++ practice.js
@@ -1,38 +1,48 @@
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
 
-function copy (mainObj) {
-let objCopy = {};
-let key 
-for (key in mainObj) {
-    objCopy[key] = mainObj[key]
-}
-return objCopy;
+newArray[1] = 'ddd'
+console.log(newArray);
+console.log(oldArray);
+
+const video = ['youtube', 'vimeo', 'rutube'],
+      blogs = ['wordpress', 'livejounal', 'blogger'],
+      internet = [...video, ...blogs, 'vk', 'ok'];  //spred 
+      console.log(internet);
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+
 }
 
-const numbers = {
-    a: 2,
-    b: 3, 
-    c: {
-        x: 7, //вложенная структура поэтому сохраняе тся на нее ссылка и изменяется и в обоих объектах
-        y: 20
+const num = [2, 5, 7];
+log(...num);
+
+const aray = ['a', 'b'];
+const newArr = [...aray];
+newArr[0] = "b";
+
+console.log(aray); // [ 'a', 'b' ]
+
+console.log(newArr); //[ 'b', 'b' ] 
+
+const q = {
+    one: 1,
+    two: 2,
+    three: {
+        x: 4,
+        y: 5
     }
 };
 
-const newNumbers = copy(numbers);
-newNumbers.c.x = 50; //поверхностная копия
-console.log(newNumbers);
-console.log(numbers);
+const newObj2 = {...q}; 
 
- const add = {
-    d: 12,
-    e: 25
- };
+/* newObj2.one = 26;
+console.log(newObj2);
+console.log(q); */
 
- const clone = Object.assign({}, add); // поверхностная копия
- clone.d = 25;
- console.log(clone);
- console.log(add);
- 
-/*  console.log(Object.assign(numbers, add));
- console.log(Object.assign(add, numbers)); */
-
-
+newObj2.three.x = 26; // поверхностная копия
+console.log(newObj2);
+console.log(q);
