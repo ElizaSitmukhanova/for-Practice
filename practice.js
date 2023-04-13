5c96e7f5468395772bb697f1596bd281110f1914
ElizaSitmukhanova <eliza.sit@mail.ru>
2023-04-13 16:52:53 +0300

    shadow copy

--- practice.js
+++ practice.js
@@ -1,31 +1,38 @@
-const arr = [1, 6, 30, 4, 5];
 
-arr.sort(compareNum);
-console.log(arr);
-
-function compareNum(a, b) {
-    return a-b
+function copy (mainObj) {
+let objCopy = {};
+let key 
+for (key in mainObj) {
+    objCopy[key] = mainObj[key]
 }
-//arr[99] = 0;
-//console.log(arr.length);
-//console.log(arr)
-//arr.pop()
-//arr.push(15)
-//console.log(arr);
-
-/* arr.forEach(function(item, i, arr){
-    console.log(`${i}: ${item} inside array ${arr}`)
-});
-for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
+return objCopy;
 }
 
-for ( let value of arr) {
-    console.log(value)
-} */
+const numbers = {
+    a: 2,
+    b: 3, 
+    c: {
+        x: 7, //вложенная структура поэтому сохраняе тся на нее ссылка и изменяется и в обоих объектах
+        y: 20
+    }
+};
 
-/* const str = prompt("", '');
-const products = str.split(', ');
+const newNumbers = copy(numbers);
+newNumbers.c.x = 50; //поверхностная копия
+console.log(newNumbers);
+console.log(numbers);
 
-console.log(products.sort()) */
-/* console.log(products.join('!')); */
+ const add = {
+    d: 12,
+    e: 25
+ };
+
+ const clone = Object.assign({}, add); // поверхностная копия
+ clone.d = 25;
+ console.log(clone);
+ console.log(add);
+ 
+/*  console.log(Object.assign(numbers, add));
+ console.log(Object.assign(add, numbers)); */
+
+
