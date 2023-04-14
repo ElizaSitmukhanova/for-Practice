485dec71db9775b0bb37cf10b477523a4e556be3
ElizaSitmukhanova <eliza.sit@mail.ru>
2023-04-14 17:20:48 +0300

    1314 code

--- practice.js
+++ practice.js
@@ -1,4 +1,4 @@
-/* const someString = 'This is some strange string';
+ const someString = 'This is some strange string';
 
 function reverse(str) {
     if (typeof (str) !== "string") {
@@ -8,9 +8,9 @@ function reverse(str) {
     return console.log(arr);
 }
 
-reverse(someString); */
+reverse(someString); 
 
-const baseCurrencies = ['USD', 'EUR'];
+ const baseCurrencies = ['USD', 'EUR'];
 const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
 
 function availableCurr(arr, missingCurr) {
@@ -26,3 +26,85 @@ function availableCurr(arr, missingCurr) {
 }
 
    availableCurr([...baseCurrencies, ...additionalCurrencies], 'EUR');
+
+
+const shoppingMallData = {
+    shops: [
+        {
+            width: 10,
+            length: 5
+        },
+        {
+            width: 15,
+            length: 7
+        },
+        {
+            width: 20,
+            length: 5
+        },
+        {
+            width: 8,
+            length: 10
+        }
+    ],
+    height: 5,
+    moneyPer1m3: 30,
+    budget: 50000
+};
+
+function isBudgetEnough(data) {
+    let square = 0;
+    let volume = 0;
+    data.shops.forEach(shop => {
+        square += shop.width * shop.length;
+    });
+
+    volume = square * data.height;
+    let totalConsumption = volume * data.moneyPer1m3;
+
+    if (data.budget - totalConsumption >= 0) {
+        return console.log('Бюджета достаточно');
+    } else {
+        return console.log('Бюджета недостаточно');
+    }
+}
+
+isBudgetEnough(shoppingMallData);
+
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris',  'Takesi', 'Sam'];
+
+function sortStudentsByGroups(arr) {
+   let newArr = arr.sort();
+   let oneGr = [];
+   let twoGr = [];
+   let threeGr = [];
+   let fourGr = 'Оставшиеся студенты:'
+   oneGr = newArr.slice(0, 3);
+   twoGr = newArr.slice(3, 6);
+   threeGr = newArr.slice(6, 9);
+   fourGr = newArr.slice(9)
+
+   return console.log([oneGr, twoGr, threeGr, `Оставшиеся студенты: ${fourGr.length === 0 ? '-' : fourGr.join(', ')}`]);
+}
+
+sortStudentsByGroups(students);
+
+
+// или такой способ 
+function sortStudentsByGroups(arr) {
+    arr.sort();
+    const a = [], b = [], c = [], rest = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i < 3) {
+            a.push(arr[i]);
+        } else if (i < 6) {
+            b.push(arr[i]);
+        } else if (i < 9) {
+            c.push(arr[i]);
+        } else {
+            rest.push(arr[i]);
+        }
+    }
+    return [a,b,c, `Оставшиеся студенты: ${rest.length === 0 ? '-' : rest.join(', ')}`]
+}
