6254cf8b0e02ae272db9e8dd4ae99af5a75d7871
ElizaSitmukhanova <eliza.sit@mail.ru>
2023-04-15 18:28:40 +0300

    dinamic tipization

--- practice.js
+++ practice.js
@@ -1,110 +1,50 @@
- const someString = 'This is some strange string';
 
-function reverse(str) {
-    if (typeof (str) !== "string") {
-        return console.log('Error');
-    }
-    let arr = str.split('').reverse().join('');
-    return console.log(arr);
-}
+//1
+console.log(typeof(String(null)));
 
-reverse(someString); 
+//2
+console.log(typeof(null + ''));
 
- const baseCurrencies = ['USD', 'EUR'];
-const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
+const num = 5;
 
-function availableCurr(arr, missingCurr) {
-    let str = '';
-    arr.length === 0 ? str = 'нет доступных валют' : str = 'Доступные валюты:\n';
+console.log('https://vk.com/catalog/' + num);
 
-    arr.forEach(function(element, index) {
-        if (missingCurr !== element) {
-            str+=`${element}\n`;
-        }
-    })
-  return console.log(str)
-}
+const fontSize = 26 + 'px';
+console.log(fontSize);
 
-   availableCurr([...baseCurrencies, ...additionalCurrencies], 'EUR');
+// to number
 
+//1
+console.log(typeof(Number('4')));
 
-const shoppingMallData = {
-    shops: [
-        {
-            width: 10,
-            length: 5
-        },
-        {
-            width: 15,
-            length: 7
-        },
-        {
-            width: 20,
-            length: 5
-        },
-        {
-            width: 8,
-            length: 10
-        }
-    ],
-    height: 5,
-    moneyPer1m3: 30,
-    budget: 50000
-};
+//2
+console.log(typeof((+'4')));
 
-function isBudgetEnough(data) {
-    let square = 0;
-    let volume = 0;
-    data.shops.forEach(shop => {
-        square += shop.width * shop.length;
-    });
+//3
+console.log(parseInt('15px', 10)); //15
 
-    volume = square * data.height;
-    let totalConsumption = volume * data.moneyPer1m3;
+let answer = +prompt('Hello', '');
 
-    if (data.budget - totalConsumption >= 0) {
-        return console.log('Бюджета достаточно');
-    } else {
-        return console.log('Бюджета недостаточно');
-    }
-}
+ // to boolean
 
-isBudgetEnough(shoppingMallData);
+ //0, '', null , undefined, false, NaN;
 
-const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris',  'Takesi', 'Sam'];
+ //1
+ let switcher = null;
 
-function sortStudentsByGroups(arr) {
-   let newArr = arr.sort();
-   let oneGr = [];
-   let twoGr = [];
-   let threeGr = [];
-   let fourGr = 'Оставшиеся студенты:'
-   oneGr = newArr.slice(0, 3);
-   twoGr = newArr.slice(3, 6);
-   threeGr = newArr.slice(6, 9);
-   fourGr = newArr.slice(9)
+ if( switcher) {
+    console.log("Working...");
+ }
 
-   return console.log([oneGr, twoGr, threeGr, `Оставшиеся студенты: ${fourGr.length === 0 ? '-' : fourGr.join(', ')}`]);
-}
+ switcher = 1;
+ if( switcher) {
+    console.log("Working...");
+ }
 
-sortStudentsByGroups(students);
+ //2
+ 
+console.log(typeof(Boolean('4')));
 
+//3
+console.log(typeof((!!'4')));
 
-// или такой способ 
-function sortStudentsByGroups(arr) {
-    arr.sort();
-    const a = [], b = [], c = [], rest = [];
-
-    for (let i = 0; i < arr.length; i++) {
-        if (i < 3) {
-            a.push(arr[i]);
-        } else if (i < 6) {
-            b.push(arr[i]);
-        } else if (i < 9) {
-            c.push(arr[i]);
-        } else {
-            rest.push(arr[i]);
-        }
-    }
-    return [a,b,c, `Оставшиеся студенты: ${rest.length === 0 ? '-' : rest.join(', ')}`]
-}
