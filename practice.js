595d9b50b39c77a04635990fe86cd560966fec7f
ElizaSitmukhanova <eliza.sit@mail.ru>
2023-04-19 16:42:04 +0300

    classList and className(old)

--- practice.js
+++ practice.js
@@ -1,15 +1,24 @@
 "use strict";
 
-const p = document.querySelectorAll('p');
+const btns = document.querySelectorAll('button');
+/* console.log(btns[0].classList.length); //количество классов 2
+console.log(btns[0].classList.item(0)); // blue name of class
+console.log(btns[1].classList.length); // 0 классов
+console.log(btns[1].classList.add("red"));
+console.log(btns[0].classList.remove("blue"));
+console.log(btns[0].classList.toggle("blue"));
 
-console.log(p);
+if (btns[1].classList.contains('red')) {
+    console.log('red');
+} */
 
-function loadScript(src) { //динамическая вставка
-    const script = document.createElement('script');
-    script.src = src;
-    script.async = false;
-    document.body.append(script);
-}
+btns[0].addEventListener('click', () => {
+/* if (!btns[1].classList.contains('red')) {
+    btns[1].classList.add('red');
+} else {
+    btns[1].classList.remove('red');
+} */
 
-loadScript("test.js");
-loadScript('some.js');
+btns[1].classList.toggle('red');
+});
+
