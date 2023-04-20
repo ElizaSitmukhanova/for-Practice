253040ea7f2ef1a943f4ed0c6552d14c3a6a9786
ElizaSitmukhanova <eliza.sit@mail.ru>
2023-04-20 13:13:23 +0300

    delegation events

--- practice.js
+++ practice.js
@@ -1,6 +1,8 @@
 "use strict";
 
-const btns = document.querySelectorAll('button');
+const btns = document.querySelectorAll('button'),
+    wrapper = document.querySelector('.btn-block');
+
 /* console.log(btns[0].classList.length); //количество классов 2
 console.log(btns[0].classList.item(0)); // blue name of class
 console.log(btns[1].classList.length); // 0 классов
@@ -12,7 +14,7 @@ if (btns[1].classList.contains('red')) {
     console.log('red');
 } */
 
-btns[0].addEventListener('click', () => {
+ btns[0].addEventListener('click', () => {
 /* if (!btns[1].classList.contains('red')) {
     btns[1].classList.add('red');
 } else {
@@ -20,5 +22,32 @@ btns[0].addEventListener('click', () => {
 } */
 
 btns[1].classList.toggle('red');
+}); 
+
+/* wrapper.addEventListener('click', (event) => {
+    if(event.target && event.target.classList.contains('blue')) {
+        console.log('Hello');
+    }
+}) */
+
+/* wrapper.addEventListener('click', (event) => {
+    if(event.target && event.target.tagName == "BUTTON") {
+        console.log('Hello');
+    }
+}); */
+
+//перебором пропускает кнопки которыебыли добавлены динамическиб то есть не сработает на посл кнопку
+/* btns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        console.log('Hello');
+    });
+}); */
+wrapper.addEventListener('click', (event) => {
+    if(event.target && event.target.matches('button.red')) {
+        console.log('Hello');
+    }
 });
 
+const btn = document.createElement('button');
+btn.classList.add('red');
+wrapper.append(btn);
