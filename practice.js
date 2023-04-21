ed7c8ad18e6e762ad916aba4873d76353455ce18
ElizaSitmukhanova <eliza.sit@mail.ru>
2023-04-21 14:14:52 +0300

    WeakSet

--- practice.js
+++ practice.js
@@ -1,41 +1,15 @@
+ let messages = [
+    {text: "Hello", from: 'Alex'},
+    {text: "aha", from: 'Tim'},
+    {text: "aloha", from: 'Kira'}
+ ];
 
-/* let user = {
-    name: 'Ivan'
-}; */
+ let readMessages = new WeakSet();
 
-/* let map = new Map();
-map.set(user, 'data');
+ readMessages.add(messages[0]);
+ //readMessages.add(messages[1]);
 
-user = null;
-console.log(map.keys()); */
+ readMessages.add(messages[0]);
+messages.shift();
+ console.log(readMessages.has(messages[0]))
 
-/* let map = new WeakMap();
-map.set(user, 'data');
-
-user = null;
-console.log(map.has(user)); //false
-console.log(map);  */
-/* const arr = [user];
-
-console.log(user);
-console.log(arr[0]); */  
-
-let cache = new WeakMap();
-
-function cacheUser(user) {
-    if(!cache.has(user)) {
-        cache.set(user, Date.now());
-    }
-    return cache.get(user);
-}
-
-let lena = {name: "Elena"};
-let Tima = {name: "Tima"};
-
-cacheUser(lena);
-cacheUser(Tima);
-
-lena = null;
-
-console.log(cache.has(lena));
-console.log(cache.has(Tima));
