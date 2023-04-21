67e315dd825cb26391246f59d701fe0bcc788375
ElizaSitmukhanova <eliza.sit@mail.ru>
2023-04-21 12:55:37 +0300

    Map

--- practice.js
+++ practice.js
@@ -1,35 +1,63 @@
+'use strict';
 
-const btn = document.querySelector('.btn');
-let timerId,
-i = 0;
-
-function myAnimation() {
-    const elem = document.querySelector('.box');
-    let pos = 0;
-
-    const id = setInterval(frame, 10);
-    function frame() {
-        if (pos == 300) {
-            clearInterval(id);
-        } else {
-            pos++;
-            elem.style.top = pos + 'px';
-            elem.style.left = pos + 'px';
-        }
+const user = {
+    name: 'Alex',
+    surname: 'Smith',
+    birthday: '20/12/1998',
+    showMyPublicData: function() {
+        console.log(`${this.name} ${this.surname}`);
     }
-}
-btn.addEventListener('click', myAnimation);
+};
 
+//console.log(typeof(Object.keys(user)[0]));
+// получаем карты из объекта
+const userMap = new Map(Object.entries(user)); 
+
+// получаем объект из карты
+const newUserObj = Object.fromEntries(userMap);
+console.log(newUserObj);
+
+
+const shops = [
+    {rise: 500},
+    {oil: 200},
+    {bread: 50},
+];
+const budget= [5000, 1500, 25000];
+
+const map = new Map([
+    [{paper: 400}, 8000]
+]);
+
+shops.forEach((shop, i) => {
+    map.set(shop, budget[i]);
+});
+
+/* map.set(shops[0], 5000);
+map.set(shops[1], 25000);
+map.set(shops[2], 35000); */
+console.log(map);
+/* console.log(map.get(shops[0]));
+console.log(map.has(shops[0])); */
+/* map.delete(key);
+map.clear();
+map.size;
+map.key() */
+/* const gooods = []; получить ключи массива
+for (let shop of map.keys()) {
+    gooods.push(Object.keys(shop)[0]);
+}
+
+console.log(gooods); */
+
+/* for (let price of map.values()) { получить значения
+    console.log(price);
+} */
 /* 
-function logger () {
-    console.log('dfjfhsd');
-    if(i === 3){
-        clearInterval(timerId);
-    } 
-    i++;
+for (let [shop, price] of map.entries()) { получить значения или ключи и вместе
+    console.log(price);
 } */
 
-/* let id = setTimeout(function log(){
-    console.log('Hello');
-    id = setTimeout(log, 500);
-}, 500); */
+/* map.forEach((value, key, map) => {
+    console.log(key, value);
+}); */
