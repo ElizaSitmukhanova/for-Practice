c5c197312066ec5c4dde00990885db287f908476
ElizaSitmukhanova <eliza.sit@mail.ru>
2023-04-23 17:53:53 +0300

    function constructors

--- practice.js
+++ practice.js
@@ -1,9 +1,21 @@
-const box = document.querySelector('.box');
+function User(name, id) {
+    this.name = name;
+    this.id = id;
+    this.human = true;
+    this.hello = function() {
+        console.log(`Hello, ${this.name}`);
+    };
+}
 
-let obserer = new MutationObserver(mutationRecords => {
-    console.log(mutationRecords);
-});
+User.prototype.exit = function(name) {
+    console.log(`Пользователь ${this.name} ушел`);
+};
 
-obserer.observe(box, {
-    childList: true,
-})
+const eliza = new User('Eliza', 2);
+const timur = new User('Timur', 23);
+console.log(eliza);
+console.log(timur);
+eliza.hello();
+timur.hello();
+
+eliza.exit();
