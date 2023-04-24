4725ed0cf2181000bb03e72a9d6fa047c7251808
ElizaSitmukhanova <eliza.sit@mail.ru>
2023-04-24 13:05:17 +0300

    this and events

--- practice.js
+++ practice.js
@@ -1,70 +1,37 @@
 'use strict';
-// 1) Обычная функция: this = window, but use strict - undefined;
-function showThis(a, b) {
-console.log(this);
-function sum() {
-    console.log(this); //undefined
-    return this.a + this.b // do not work
-}
-console.log(sum());
-}
 
-showThis(5, 4); 
+const btn = document.querySelector('button');
+// сам элемент
+/* btn.addEventListener('click', function() {
+    console.log(this); 
+}) */
 
-//2) контекст у методов объекта - сам объект
+/* btn.addEventListener('click', function() {
+    this.style.backgroundColor = 'red';
+}); */
+
+
+//в стредлочной функции контекста нет, this undefined
+/* btn.addEventListener('click', () => {
+    this.style.backgroundColor = 'red';
+}); */
+
+// поэnому использует e.target
+btn.addEventListener('click', (e) => {
+   e.target.style.backgroundColor = 'red';
+});
+
 const obj = {
-    a: 20,
-    b: 15,
-    sum: function() {
-        console.log(this);
+    num: 5,
+    sayNumber: function(){
+        const say = () => {
+            console.log(this);
+        };
+        say();
     }
 };
-obj.sum();
-// но например здесь вызвается обычняа функция и она теряет контекст 
-const obj2 = {
-    a: 20,
-    b: 15,
-    sum: function() {
-       function showsNew() {
-        console.log(this);
-    } 
-    showsNew();
-    }
-};
-obj2.sum();
 
-// 3) this в конструкторах и классах - это новый экземпляр объектаж
+obj.sayNumber();
 
-function User(name, id) {
-    this.name = name;
-    this.id = id;
-    this.human = true;
-    this.helloo= function() {
-        console.log(`Hello ${this.name}`);
-        
-    };
-}
-
-let eliza = new User("Eliza", 25);
-console.log(typeof(eliza));
-//4)ручная привязка this: call, apply, bind;
-
-function sayName(surname) {
-    console.log(this);
-    console.log(this.name + surname);
-}
-
-const user = {
-    name: "Kira",
-};
-//разница в синтаксисе
-sayName.call(user, 'Smith'); 
-sayName.apply(user, ['Smith']);
-
-//создает новую функцию
-function count(num) {
-    return this*num;
-}
-
-const double = count.bind(2); //уходит в this
-console.log(double(13)); // num 13
+const double = a =>  a * 2;
+console.log(double(5));
