2a361989bb0f7369076e85bf4b55bcbacac97e18
ElizaSitmukhanova <eliza.sit@mail.ru>
2023-04-27 16:40:03 +0300

    promise race and all

--- practice.js
+++ practice.js
@@ -1,36 +1,18 @@
 'use strict';
 
-console.log('Request of data');
-
-const req = new Promise(function (resolve, reject) {
-    setTimeout(() => {
-        console.log('Data is being prepared');
-
-        const product = {
-            name: 'TV',
-            price: 25000
-        };
-
-         resolve(product);
-    }, 2000);
-});
-
-req.then((product) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            product.status = 'order';
-             resolve(product);
-        }, 2000);
+const test = time => {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(), time);
     });
+};
 
-}).then(data => {
-    data.modify = true;
-    return data;
-}).then(data => {
-    console.log(data);
-}).catch(() => {
-    console.error('Error occurred');
-}).finally(() => {
-    console.log('Finally');
-});
+/* test(1000).then(() => console.log('1000ms'));
+test(2000).then(() => console.log('2000ms')); */
  
+/* Promise.all([test(1000), test(2000)]).then(() => {
+    console.log('All');
+}); */
+
+Promise.race([test(1000), test(2000)]).then(() => {
+    console.log('All');
+});
