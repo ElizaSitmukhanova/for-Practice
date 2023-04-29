1395bf5346c753e3edc759fc2ac7a834aa529468
ElizaSitmukhanova <eliza.sit@mail.ru>
2023-04-29 15:45:54 +0300

    another example methods

--- practice.js
+++ practice.js
@@ -1,50 +1,21 @@
 'use strict';
-const films = [
-    {
-        name: 'Titanic',
-        rating: 9
-    },
-    {
-        name: 'Die hard 5',
-        rating: 5
-    },
-    {
-        name: 'Matrix',
-        rating: 8
-    },
-    {
-        name: 'Some bad film',
-        rating: 4
-    }
+const funds = [
+    {amount: -1400},
+    {amount: 2400},
+    {amount: -1000},
+    {amount: 500},
+    {amount: 10400},
+    {amount: -11400}
 ];
 
+const getPositiveIncomeAmount = (data) => {
+    let positiv = data.filter(item => item.amount > 0 );
+    let sum = positiv.reduce((s, i) => s + i.amount, 0);
+    return sum;
+};
+console.log(getPositiveIncomeAmount(funds));
 
-/* function showGoodFilms(arr) {
-    let res = arr.filter(item=> item.rating >= 8);
-   return res;
-}
-
-console.log(showGoodFilms(films));
-
-function showListOfFilms(arr) {
-    let res = arr.map(item=> item.name).join(', ');
-   return res; 
-}
-console.log(showListOfFilms(films));
- */
-function setFilmsIds(arr) {
-return arr.map((film, item) =>{
-        film.id = item; 
-    return film;
- });
-}
-console.log( setFilmsIds(films));
-
-const tranformedArray = setFilmsIds(films);
-
-function checkFilms(arr) {
- return arr.every(film => film.id || film.id === 0 );
-}
-
-console.log(checkFilms(tranformedArray));
- 
+const getTotalIncomeAmount = (data) => {
+  return data.some(item => item.amount < 0) ? data.reduce((sum, curr) => sum +curr.amount, 0) : getPositiveIncomeAmount(data);
+};
+console.log(getTotalIncomeAmount(funds));
