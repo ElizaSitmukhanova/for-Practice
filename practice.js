c1fdd07c390ee842233096bb59f49a8625193f87
ElizaSitmukhanova <eliza.sit@mail.ru>
2023-05-05 13:09:42 +0300

    encapsulation

--- practice.js
+++ practice.js
@@ -1,20 +1,89 @@
-/* new RegExp('pattern', 'flags');
-/pattern/f */
+//нарушение инкапсуляции
+/* function User (name, age) {
+    this.age = age;
+    this.name = name;
 
-/* let answer = prompt("Введите свое имя");
-const req = /n/ig;
+    this.say = function () {
+        console.log(`Это пользователь с именем ${this.name} и возрастом ${this.age}`);
+    };
+}
 
-console.log(answer.search(req));
-console.log(answer.match(req)); */
+const ela = new User('Ela', 24);
 
-/* let pass = prompt('Password');
+console.log(ela.name);
+ela.say();
 
-console.log(pass.replace(/./g, '*')); */
+ela.age = 25;
+ela.name = 'Gafg';
+ela.say(); */
 
-const str = "My name is RT45";
+//инкапсуляция в функциях конструкторах
+/* function User (name, age) {
+    this.age = age;
+    let userName = name;
 
-console.log(str.match(/\w\w\d\d/i));
-console.log(str.match(/\W/ig));
+    this.say = function () {
+        console.log(`Это пользователь с именем ${userName} и возрастом ${this.age}`);
+    };
+    this.getName = function () {
+        return userName;
+    };
+    this.setName = function (name) {
+        if(typeof name === 'string' && name.length > 0 && name.length < 5 ) {
+          userName = name;  
+        } else {
+            console.log('Error');
+        }   
+    };
+}
 
+const ela = new User('Ela', 24);
 
+console.log(ela.userName);
+
+console.log(ela.getName());
+
+ela.setName('dsjgidsjgkgjdf');
+ela.setName('dc');
+ela.say();
+console.log(ela.getName()); */
+
+//инкапсуляция в функциях классах
+
+class User  {
+    constructor (name, age) {
+        this.age = age;
+        this._userName = name;
+    }
+    #surName = 'Sit';
+    say () {
+        console.log(`Это пользователь с именем ${this._userName} ${this.#surName} и возрастом ${this.age}`);
+    }
+    get name () {
+        return this._userName;
+    }
+    set name (name) {
+        if(typeof name === 'string' && name.length > 0 && name.length < 5 ) {
+          this._userName = name;  
+        } else {
+            console.log('Error');
+        }   
+    }
+    get surName () {
+        return this.#surName;
+    }
+    set surName (surName) {
+        this.#surName = surName;
+    }
+}
+
+const ela = new User('Ela', 24);
+/* ela.userName = 'Greta';
+console.log(ela.name);
+
+ela.name = 'Wita'; */
+/* console.log(ela.name); */
+ela.surName = 'Sitmukhanova';
+console.log(ela.surName);
+ela.say();
 
