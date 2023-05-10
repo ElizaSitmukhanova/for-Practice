7151aa4ef854cb0d2a92fd42605eeef95ddbad27
ElizaSitmukhanova <eliza.sit@mail.ru>
2023-05-10 14:49:58 +0300

    es6 module export import

---
+++ js/script.js
@@ -0,0 +1,6 @@
+import * as data  from './test.js';
+import sayHi from './test.js';
+
+console.log(`I have ${data.one} and ${data.two}`);
+
+sayHi();
---
+++ js/test.js
@@ -0,0 +1,8 @@
+let one = 1;
+export {one};
+
+export let two = 2;
+
+export default function sayHi() {
+    console.log('Hello, my dear');
+}
