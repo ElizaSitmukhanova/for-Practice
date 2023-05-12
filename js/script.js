846d6a00933866209f28161c11e88a311600e4d2
ElizaSitmukhanova <eliza.sit@mail.ru>
2023-05-12 13:33:54 +0300

    function generator

--- js/script.js
+++ js/script.js
@@ -1,18 +1,37 @@
-import $ from 'jquery';
+function* generator() {
+  yield 'S';
+  yield 'k';
+  yield 'm';
+  yield 'n';
+  yield 'So';
+}
 
-$(document).ready(function(){
-  $('.list-item:first').hover(function(){
-    $(this).toggleClass('active');
-  });
-  $('.list-item:eq(2)').on('click', function(){
-     $('.image:even').fadeToggle();
-  });
-  $('.list-item:eq(4)').on('click', function(){
-    $('.image:odd').animate(
-      {
-        opacity: "toggle",
-        height: "toggle"
-      },
-    2000);
- });
-  });
+const str = generator();
+
+console.log(str.next());
+console.log(str.next().value); 
+/* console.log(str.next());
+console.log(str.next());
+console.log(str.next());
+console.log(str.next());
+console.log(str.next()); */
+
+function* count(n) {
+  for (let i = 0; i<n; i++) {
+    yield i;
+  }
+}
+
+const counter = count(7);
+
+/* console.log(counter.next().value);
+console.log(counter.next().value);
+console.log(counter.next().value);
+console.log(counter.next().value);
+console.log(counter.next().value);
+console.log(counter.next().value);
+ */
+
+for (let k of count(7)) {
+  console.log(k);
+}
