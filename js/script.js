155bc7c01d1935be3675df5011bf22b5c098cc1d
ElizaSitmukhanova <eliza.sit@mail.ru>
2023-05-12 16:32:18 +0300

    function composition task

--- js/script.js
+++ js/script.js
@@ -1,30 +1,64 @@
-const { _ } = require("core-js");
+/* const compose = (a, b, c) => (x) => a(b(c(x)));
+const multiply20 = (price) => price * 20;
+const divide100 = (price) => price / 100;
+const normalizePrice = (price) => price.toFixed(2);
+const discount = compose(normalizePrice, divide100, multiply20);
+console.log(discount(200.0)); */
 
-const btnPhone = document.querySelector('#iphone'),
-      btnMacBook =document.querySelector('#macbook'),
-      images = document.querySelectorAll('img');
+
+const multiply20 = (price) => price * 20;
+const divide100 = (price) => price / 100;
+const normalizePrice = (price) => price.toFixed(2);
+
+const compose =
+  (...fns) =>
+  (x) =>
+    fns.reduceRight((res, fn) => fn(res), x);
+
+const discount = compose(normalizePrice, divide100, multiply20);
+console.log(discount(200.0));
+
+const multiply20 = (price) => price * 20;
+const divide100 = (price) => price / 100;
+const normalizePrice = (price) => price.toFixed(2);
+
+const newComp = (...fns) =>
+  (x) => {
+    return fns.reduceRight(function(res, fn) {
+      return fn(res), x);
+  }
+};
+
+const disc = newComp(normalizePrice, divide100, multiply20);
+console.log(disc(200.0));
 
 
-      let phoneAnimation;
-      btnPhone.addEventListener('click', () => {
-        if(!phoneAnimation){
-          phoneAnimation = images[0].animate([
-            {transform: 'translateY(0) rotate(0deg',
-          filter: 'opacity(100%)'},
-            {transform: 'translateY(200px) rotate(180deg', 
-            filter: 'opacity(50%)'},
-            {transform: 'translateY(-20px) rotate(270deg',
-            filter: 'opacity(75%)'},
-            {transform: 'translateY(0) rotate(360deg',
-            filter: 'opacity(100%)'},
-          ],{
-            duration: 3000,
-            iterations: Infinity
-          });
-        } else if (phoneAnimation.playState === 'paused') {
-          phoneAnimation.play();
-        } else {
-          phoneAnimation.pause();
-        }
-      });
- 
+
+
+
+
+const add1 = function(a){return a + 1}
+const addAll3 = function(a,b,c){return a + b + c};
+
+const composeWithArgs = (...fns)  => fns.reduceRight((f,g) => (...arg) => g(f(...arg)));
+console.log(composeWithArgs(add1,addAll3)(1,2,3));
+
+const newcompose = (...fns) => {
+  return fns.reduceRight((f,g) => (...arg) => g(f(...arg)));
+};
+console.log(newcompose(add1,addAll3)(1,2,3));
+
+/* Метод reduceRight принимает коллбэк функцию с
+previousValue -Значение, возвращённое предыдущим выполнением функции callback, 
+либо значение initialValue, если оно предоставлено.
+currentValue- Текущий обрабатываемый элемент массива.
+Что в наших реалиях называется res и fn.  Теперь перейдем к fns. Это массив с функциями.
+При первом запуске res будет равен х, так как второй аргумент у reduceRight есть. 
+То есть, это начальное значение, тот аргумент, который передавался в первую функцию. 
+fn - это первая функция в массиве. Получается, что
+fn(res) - это запуск первой переданной (вызванной, той что справа) функции с начальным аргументом.
+ А каждые последующие запуски в res помещается полученный результат.
+
+ composeWithArgs = (...fns) => fns.reduceRight((f, g) => (...args) => g(f(...args)));
+изначально мы передаём только массив с функциями ...fns.
+с ним начинает работать fns.reduceRight в которой g это текущий элемент массива, f предыдущий. */
